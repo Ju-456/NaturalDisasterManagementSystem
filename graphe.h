f226e6d43cf5e225ff2d5c0e5040c182672a23d3
Juliena <juliena456@gmail.com>
2025-04-11 21:17:34 +0200

    add 'road_capacity' in Road's structur

--- graphe.h
+++ graphe.h
@@ -9,12 +9,19 @@ typedef struct {
     int state;        // 0, 1, 2
     int traffic;      // 0 or 1
     float weight;     // weight of the road 
+    int road_capacity;
 } Road;
 
 typedef struct {
     int type;              // 0, 1, 2 (city, hospital, warehouse)
     int storage;           
-    int storage_limit;     // maximum storage capacity
+    int storage_capacity;     // maximum storage capacity
 } Vertex; 
+
+typedef struct {
+    int ambulance;
+    int truck;
+    int vehicle_capacity;
+} Vehicle;
 
 #endif
